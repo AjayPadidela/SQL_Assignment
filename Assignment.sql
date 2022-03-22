071c3ef2ffef11b8998f58c1541b0f7bea345a6f
AjayPadidela <97826915+AjayPadidela@users.noreply.github.com>
2022-03-22 23:02:15 +0530

    Update Assignment.sql

--- Assignment.sql
+++ Assignment.sql
@@ -1,7 +1,7 @@
 create database edyodadb;
 use edyodadb;
 
--- Creating table1: SalesPeople
+--- table1: SalesPeople
 create table SalesPeople(
 Snum int not null,
 Sname varchar(255) not null,
@@ -19,7 +19,7 @@ insert into SalesPeople values
 (1003,'Axelrod','Newyork',.10);
 
 
--- Creating table2: Customers
+-- table2: Customers
 create table Customers(
 Cnum int not null,
 Cname varchar(255) not null,
@@ -39,7 +39,7 @@ insert into Customers values
 (2007,'Pereira','Rome',1004);
 
 
--- Creating table3: Orders
+-- table3: Orders
 create table Orders(
 Onum int not null,
 Amt decimal(10,2) not null,
@@ -52,8 +52,7 @@ foreign key(Snum) references SalesPeople(Snum)
 );
 
 insert into Orders values
-(3001,18.69,'1990-10-03',2008,1007);
-insert into Orders values
+(3001,18.69,'1990-10-03',2008,1007),
 (3003,767.19,'1990-10-03',2001,1001),
 (3002,1900.10,'1990-10-03',2007,1004),
 (3005,5160.45,'1990-10-03',2003,1002),
@@ -64,25 +63,20 @@ insert into Orders values
 (3010,1309.95,'1990-10-06',2004,1002),
 (3011,9891.88,'1990-10-06',2006,1001);
 
-
 -- Count the number of Salesperson whose name begin with ‘a’/’A’.
 select count(Sname) as Count from SalesPeople where Sname like 'A%' or Sname like 'a%';
-
 
 -- Display all the Salesperson whose all orders worth is more than Rs. 2000
 select s.Sname, sum(o.Amt) from SalesPeople as s JOIN Orders as o on s.Snum=o.Snum group by s.Sname having sum(o.Amt)>2000;
 
-
 -- Count the number of Salesperson belonging to Newyork.
 select count(Sname) as SalesPerson_from_Newyork from SalesPeople where City='Newyork';
  
-
 -- Display the number of Salespeople belonging to London and belonging to Paris.
 select count(*) as SalesPerson_from_London from SalesPeople where City='London';
 select count(*) as SalesPerson_from_Paris from SalesPeople where City='Paris';
 -- for London or Paris
 select count(*) as SalesPerson from SalesPeople where City='London' or City='Paris';
-
 
 -- Display the number of orders taken by each Salesperson and their date of orders. 
 select s.Sname as SalesPerson, o.Onum as OrderNumber, o.Odate as OrderDate from SalesPeople as s join Orders as o on s.Snum=o.Snum;
